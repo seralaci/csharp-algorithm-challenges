b475c62d85bb0cd684b2941a6431e768397141ab
László Séra <laszlo.sera@gmail.com>
2018-06-11 23:19:45 +0200

    More tests added to "Roman to Integer"

--- LeetCode/Challenges.LeetCode.Tests/RomanToIntegerTests.cs
+++ LeetCode/Challenges.LeetCode.Tests/RomanToIntegerTests.cs
@@ -9,9 +9,17 @@ namespace Challenges.LeetCode.Tests
         public static IEnumerable<object[]> GetTestData()
         {
             yield return new object[] { "I", 1 };
+            yield return new object[] { "II", 2 };
             yield return new object[] { "III", 3 };
             yield return new object[] { "IV", 4 };
+            yield return new object[] { "V", 5 };
+            yield return new object[] { "VI", 6 };
+            yield return new object[] { "VII", 7 };
+            yield return new object[] { "VIII", 8 };
             yield return new object[] { "IX", 9 };
+            yield return new object[] { "X", 10 };
+            yield return new object[] { "XC", 90 };
+            yield return new object[] { "C", 100 };
             yield return new object[] { "LVIII", 58 };
             yield return new object[] { "MCMXCIV", 1994 };
             yield return new object[] { "MMMCMXCIX", 3999 };
