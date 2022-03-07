8791370a74838ec2e375c665550def7ad3a1cece
László Séra <laszlo.sera@gmail.com>
2022-03-07 16:58:50 +0100

    Simplified benchmark tests as it is not necessary to benchmark everything at once

--- LeetCode/Challenges.LeetCode.Tests.Performance/Program.cs
+++ LeetCode/Challenges.LeetCode.Tests.Performance/Program.cs
@@ -1,28 +1,4 @@
-﻿using System.Linq;
-using System.Reflection;
-using BenchmarkDotNet.Running;
-using static System.Console;
+﻿using BenchmarkDotNet.Running;
 
-#if DEBUG
-    WriteLine("Warning: DEBUG configuration; performance may be impacted!");
-    WriteLine();
-#endif
-
-if (args.Length == 0)
-{
-    WriteLine("Optional arguments:");
-    WriteLine("  --all : run all benchmarks");
-    WriteLine();
-}
-
-if (args.Any(a => a == "--all"))
-{
-    WriteLine("Running full benchmarks suite");
-
-    BenchmarkRunner.Run(Assembly.GetEntryAssembly());
-}
-else
-{
-    BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
-}
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         
